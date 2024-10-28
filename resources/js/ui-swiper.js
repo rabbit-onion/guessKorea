4d9464d2ba0546dd838b66a6bf00974b7d5199ec
Onion <csr1000@naver.com>
2024-10-28 17:52:41 +0900

    intro 작업 1

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,29 +1,41 @@
-var swiper = new Swiper(".slide-intro", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  // autoplay: true,
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiperIntro = new Swiper(".slide-intro", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
-    el: ".slide-intro .swiper-pagination",
+    el: ".swiper-pagination",
     clickable: true,
   },
+  on: {},
 });
 
-var swiper = new Swiper(".slide-studio", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".slide-studio .swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    600: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
+const swiperWrapper = document.querySelector(".swiper-wrapper");
+const playPauseButton = document.querySelector(".swiper-button-play-pause");
+
+playPauseButton.addEventListener("click", function () {
+  const span = this.querySelector("span");
+  playPauseButton.classList.toggle("clicked");
+  playPauseButton.classList.toggle("default");
+  if (swiperIntro.autoplay.running) {
+    swiperIntro.autoplay.stop();
+    swiperWrapper.setAttribute("aria-live", "polite");
+    this.setAttribute("aria-label", "슬라이드 쇼 재생");
+
+    if (span) {
+      span.textContent = "재생";
+    }
+  } else {
+    swiperIntro.autoplay.start();
+    swiperWrapper.setAttribute("aria-live", "off");
+    this.setAttribute("aria-label", "슬라이드 쇼 정지");
+
+    if (span) {
+      span.textContent = "정지";
+    }
+  }
 });
