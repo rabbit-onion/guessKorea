aecb038f1567c4c89f2186977f897b726b7a98f2
Onion <csr1000@naver.com>
2024-12-03 17:49:38 +0900

    swiper 재생/정지버튼 js 작업 중

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,16 +1,17 @@
 const swiperIntro = new Swiper(".slide-intro", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   on: {},
   loop: true,
+  secName: "slide",
 });
 
 // best arrivals section
@@ -31,80 +32,142 @@ const swiperBestImg = new Swiper(".slide-best-img", {
   on: {
     slideChangeTransitionStart: function () {},
   },
+  secName: "sec-best",
 });
 
 const swiperBestInfo = new Swiper(".slide-best-info", {
   centeredSlides: true,
   loop: true,
   speed: 1000,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  secName: "sec-best",
 });
 
 // bestImg 스와이퍼와 bestInfo 스와이퍼를 연동
 swiperBestImg.controller.control = swiperBestInfo;
 swiperBestInfo.controller.control = swiperBestImg;
 
-// swiper 재생/정지 버튼
-// ===================================================================
-const swiperWrapper = document.querySelector(".swiper-wrapper");
-const playPauseButton = document.querySelector(".swiper-button-play-pause");
-
-playPauseButton.addEventListener("click", function () {
-  const span = this.querySelector("span");
-  playPauseButton.classList.toggle("clicked");
-  playPauseButton.classList.toggle("default");
-  if (swiperIntro.autoplay.running) {
-    swiperIntro.autoplay.stop();
-    swiperWrapper.setAttribute("aria-live", "polite");
-    this.setAttribute("aria-label", "슬라이드 쇼 재생");
-
-    if (span) {
-      span.textContent = "재생";
-    }
-  } else {
-    swiperIntro.autoplay.start();
-    swiperWrapper.setAttribute("aria-live", "off");
-    this.setAttribute("aria-label", "슬라이드 쇼 정지");
-
-    if (span) {
-      span.textContent = "정지";
-    }
-  }
-});
-
 // new arrivals section
 // ====================================================
 
 const swiperNew = new Swiper(".slide-new", {
   slidesPerView: "auto",
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".sec-new-pagination",
     clickable: true,
   },
   loop: true,
+  secName: "sec-new",
 });
 
 const swiperBranding = new Swiper(".slide-branding", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   loop: true,
+  secName: "sec-branding",
 });
+
+// swiper 재생/정지 버튼 컨트롤
+// ============================================================================
+// const swiperWrapper = document.querySelector(".swiper-wrapper");
+
+const swiperList = [swiperIntro, swiperBestImg, swiperBestInfo, swiperNew, swiperBranding];
+
+swiperList.forEach((item) => {
+  const playPauseButton = document.querySelector(`.${item.passedParams.secName} .swiper-button-play-pause`);
+
+  playPauseButton.addEventListener("click", function () {
+    if (item === swiperBestImg) {
+      return;
+    }
+
+    const span = this.querySelector("span");
+    playPauseButton.classList.toggle("clicked");
+    playPauseButton.classList.toggle("default");
+
+    if (item.autoplay.running) {
+      item.autoplay.stop();
+      swiperWrapper.setAttribute("aria-live", "polite");
+      this.setAttribute("aria-label", "슬라이드 쇼 재생");
+
+      if (span) {
+        span.textContent = "재생";
+      }
+    } else {
+      item.autoplay.start();
+      swiperWrapper.setAttribute("aria-live", "off");
+      this.setAttribute("aria-label", "슬라이드 쇼 정지");
+
+      if (span) {
+        span.textContent = "정지";
+      }
+    }
+  });
+});
+
+// playPauseButton.addEventListener("click", function () {
+//   const span = this.querySelector("span");
+//   playPauseButton.classList.toggle("clicked");
+//   playPauseButton.classList.toggle("default");
+//   swiperList.map((item) => {
+//     if (item.autoplay.running) {
+//       item.autoplay.stop();
+//       swiperWrapper.setAttribute("aria-live", "polite");
+//       this.setAttribute("aria-label", "슬라이드 쇼 재생");
+
+//       if (span) {
+//         span.textContent = "재생";
+//       }
+//     } else {
+//       item.autoplay.start();
+//       swiperWrapper.setAttribute("aria-live", "off");
+//       this.setAttribute("aria-label", "슬라이드 쇼 정지");
+
+//       if (span) {
+//         span.textContent = "정지";
+//       }
+//     }
+//   });
+// });
+
+// playPauseButton.addEventListener("click", function () {
+//   const span = this.querySelector("span");
+//   playPauseButton.classList.toggle("clicked");
+//   playPauseButton.classList.toggle("default");
+//   if (swiperIntro.autoplay.running) {
+//     swiperIntro.autoplay.stop();
+//     swiperWrapper.setAttribute("aria-live", "polite");
+//     this.setAttribute("aria-label", "슬라이드 쇼 재생");
+
+//     if (span) {
+//       span.textContent = "재생";
+//     }
+//   } else {
+//     swiperIntro.autoplay.start();
+//     swiperWrapper.setAttribute("aria-live", "off");
+//     this.setAttribute("aria-label", "슬라이드 쇼 정지");
+
+//     if (span) {
+//       span.textContent = "정지";
+//     }
+//   }
+// });
