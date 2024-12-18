929e1f6234331ec7d7d02d59ee56538ff5f8c8a4
Onion <csr1000@naver.com>
2024-12-18 17:48:22 +0900

    brand page - key, intro section layout

--- resources/js/ui-aos.js
+++ resources/js/ui-aos.js
@@ -1,1 +1,10 @@
-AOS.init();
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".sec-b");
+
+  sections.forEach(function (section) {
+    section.setAttribute("data-aos", "fade-up");
+    section.setAttribute("data-aos-once", "false");
+  });
+
+  AOS.init();
+});
--- resources/js/ui-gsap.js
+++ resources/js/ui-gsap.js
@@ -1,34 +1,34 @@
 // brand-key section 애니메이션
-const ani1 = gsap.timeline();
+// const ani1 = gsap.timeline();
 
-let keySection = document.querySelector(".brand-key");
-let keyLogo = document.querySelector(".brand-key svg");
+// let keySection = document.querySelector(".brand-key");
+// let keyLogo = document.querySelector(".brand-key svg");
 
-ani1.to(keyLogo, { scale: 3, opacity: 0 });
+// ani1.to(keyLogo, { scale: 3, opacity: 0 });
 
-ScrollTrigger.create({
-  animation: ani1,
-  trigger: keySection,
-  start: "top top",
-  end: "+=200vh",
-  pin: true,
-  scrub: true,
-  pinSpacing: false,
-  markers: true,
-});
+// ScrollTrigger.create({
+//   animation: ani1,
+//   trigger: keySection,
+//   start: "top top",
+//   end: "+=200vh",
+//   pin: true,
+//   scrub: true,
+//   pinSpacing: false,
+//   markers: true,
+// });
 
 // 섹션 풀페이지 효과
-document.querySelectorAll(".sec-b").forEach((section) => {
-  gsap.from(section.querySelector("h2"), {
-    opacity: 0,
-    y: "20%", // 위로 20% 이동하면서 투명도 0에서 1로
-    duration: 1,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger: section, // 각 섹션을 트리거로 사용
-      start: "top bottom", // 화면에 나타나는 위치 (섹션의 top이 화면 bottom에 닿을 때)
-      end: "bottom top", // 섹션의 bottom이 화면 top을 지날 때
-      scrub: true, // 스크롤에 맞춰 애니메이션 진행
-    },
-  });
-});
+// document.querySelectorAll(".sec-b").forEach((section) => {
+//   gsap.from(section.querySelector("h2"), {
+//     opacity: 0,
+//     y: "20%", // 위로 20% 이동하면서 투명도 0에서 1로
+//     duration: 1,
+//     ease: "power2.out",
+//     scrollTrigger: {
+//       trigger: section, // 각 섹션을 트리거로 사용
+//       start: "top bottom", // 화면에 나타나는 위치 (섹션의 top이 화면 bottom에 닿을 때)
+//       end: "bottom top", // 섹션의 bottom이 화면 top을 지날 때
+//       scrub: true, // 스크롤에 맞춰 애니메이션 진행
+//     },
+//   });
+// });
